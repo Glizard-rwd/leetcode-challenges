980f6504d776f2cd75bae234bbcb0d06e50adc38
s3915155 <s3915155@rmit.edu.vn>
2023-06-08 15:05:26 +0700

    equilibrium index: add prefix sum solution

--- enjoyalgorithms/EquilibriumIndex.java
+++ enjoyalgorithms/EquilibriumIndex.java
@@ -1,4 +1,7 @@
 package enjoyalgorithms;
+
+import java.util.Arrays;
+
 /*
 https://www.enjoyalgorithms.com/blog/find-equilibrium-index-of-an-array/
  */
@@ -18,14 +21,25 @@ public class EquilibriumIndex {
         return -1; // equilibrium index does not exit
     }
 
-    public static void equilibriumIndPrefix(int[], int n) {
+    public static int equilibriumIndPrefix(int[] arr, int n) {
         int[] prefix = new int[n]; // declare prefix array
+        Arrays.fill(prefix, 0);
         // add element to prefix array
         for (int i = 0; i < n; i++) {
-            
+            if (i == 0) prefix[0] = arr[i];
+            else prefix[i] = arr[i] + prefix[i-1];
         }
+        int total = prefix[n-1];
+        for (int i = 0; i < n; i++) {
+            int leftSum = prefix[i] - arr[i];
+            int rightSum = total - prefix[i];
+            if (leftSum == rightSum) return i;
+        }
+        return -1;
     }
     public static void main(String[] args) {
-
+        int[] A = new int[]{-7, 1, 5, 2, -4, 3, 0};
+        System.out.println(equilibriumIndex(A, A.length));
+        System.out.println(equilibriumIndPrefix(A, A.length));
     }
 }
